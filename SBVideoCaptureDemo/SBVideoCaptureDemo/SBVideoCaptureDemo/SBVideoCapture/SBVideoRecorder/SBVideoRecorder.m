59a3deefff0d00626c36f0c1a89bf428d8410201
PandaraWen <pandara_wen@foxmail.com>
2014-10-16 15:35:12 +0800

    修改小bug

--- SBVideoCaptureDemo/SBVideoCaptureDemo/SBVideoCaptureDemo/SBVideoCapture/SBVideoRecorder/SBVideoRecorder.m
+++ SBVideoCaptureDemo/SBVideoCaptureDemo/SBVideoCaptureDemo/SBVideoCapture/SBVideoRecorder/SBVideoRecorder.m
@@ -164,98 +164,100 @@
  */
 - (void)mergeAndExportVideosAtFileURLs:(NSArray *)fileURLArray
 {
-    NSError *error = nil;
-
-    CGSize renderSize = CGSizeMake(0, 0);
-    
-    NSMutableArray *layerInstructionArray = [[NSMutableArray alloc] init];
-    
-    AVMutableComposition *mixComposition = [[AVMutableComposition alloc] init];
-    
-    CMTime totalDuration = kCMTimeZero;
-    
-    //先去assetTrack 也为了取renderSize
-    NSMutableArray *assetTrackArray = [[NSMutableArray alloc] init];
-    NSMutableArray *assetArray = [[NSMutableArray alloc] init];
-    for (NSURL *fileURL in fileURLArray) {
-        AVAsset *asset = [AVAsset assetWithURL:fileURL];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSError *error = nil;
         
-        if (!asset) {
-            continue;
+        CGSize renderSize = CGSizeMake(0, 0);
+        
+        NSMutableArray *layerInstructionArray = [[NSMutableArray alloc] init];
+        
+        AVMutableComposition *mixComposition = [[AVMutableComposition alloc] init];
+        
+        CMTime totalDuration = kCMTimeZero;
+        
+        //先去assetTrack 也为了取renderSize
+        NSMutableArray *assetTrackArray = [[NSMutableArray alloc] init];
+        NSMutableArray *assetArray = [[NSMutableArray alloc] init];
+        for (NSURL *fileURL in fileURLArray) {
+            AVAsset *asset = [AVAsset assetWithURL:fileURL];
+            
+            if (!asset) {
+                continue;
+            }
+            
+            [assetArray addObject:asset];
+            
+            AVAssetTrack *assetTrack = [[asset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0];
+            [assetTrackArray addObject:assetTrack];
+            
+            renderSize.width = MAX(renderSize.width, assetTrack.naturalSize.height);
+            renderSize.height = MAX(renderSize.height, assetTrack.naturalSize.width);
         }
         
-        [assetArray addObject:asset];
+        CGFloat renderW = MIN(renderSize.width, renderSize.height);
         
-        AVAssetTrack *assetTrack = [[asset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0];
-        [assetTrackArray addObject:assetTrack];
+        for (int i = 0; i < [assetArray count] && i < [assetTrackArray count]; i++) {
+            
+            AVAsset *asset = [assetArray objectAtIndex:i];
+            AVAssetTrack *assetTrack = [assetTrackArray objectAtIndex:i];
+            
+            AVMutableCompositionTrack *audioTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeAudio preferredTrackID:kCMPersistentTrackID_Invalid];
+            [audioTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, asset.duration)
+                                ofTrack:[[asset tracksWithMediaType:AVMediaTypeAudio] objectAtIndex:0]
+                                 atTime:totalDuration
+                                  error:nil];
+            
+            AVMutableCompositionTrack *videoTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeVideo preferredTrackID:kCMPersistentTrackID_Invalid];
+            
+            [videoTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, asset.duration)
+                                ofTrack:assetTrack
+                                 atTime:totalDuration
+                                  error:&error];
+            
+            //fix orientationissue
+            AVMutableVideoCompositionLayerInstruction *layerInstruciton = [AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:videoTrack];
+            
+            totalDuration = CMTimeAdd(totalDuration, asset.duration);
+            
+            CGFloat rate;
+            rate = renderW / MIN(assetTrack.naturalSize.width, assetTrack.naturalSize.height);
+            
+            CGAffineTransform layerTransform = CGAffineTransformMake(assetTrack.preferredTransform.a, assetTrack.preferredTransform.b, assetTrack.preferredTransform.c, assetTrack.preferredTransform.d, assetTrack.preferredTransform.tx * rate, assetTrack.preferredTransform.ty * rate);
+            layerTransform = CGAffineTransformConcat(layerTransform, CGAffineTransformMake(1, 0, 0, 1, 0, -(assetTrack.naturalSize.width - assetTrack.naturalSize.height) / 2.0));//向上移动取中部影响
+            layerTransform = CGAffineTransformScale(layerTransform, rate, rate);//放缩，解决前后摄像结果大小不对称
+            
+            [layerInstruciton setTransform:layerTransform atTime:kCMTimeZero];
+            [layerInstruciton setOpacity:0.0 atTime:totalDuration];
+            
+            //data
+            [layerInstructionArray addObject:layerInstruciton];
+        }
         
-        renderSize.width = MAX(renderSize.width, assetTrack.naturalSize.height);
-        renderSize.height = MAX(renderSize.height, assetTrack.naturalSize.width);
-    }
-    
-    CGFloat renderW = MIN(renderSize.width, renderSize.height);
-    
-    for (int i = 0; i < [assetArray count] && i < [assetTrackArray count]; i++) {
-
-        AVAsset *asset = [assetArray objectAtIndex:i];
-        AVAssetTrack *assetTrack = [assetTrackArray objectAtIndex:i];
+        //get save path
+        NSURL *mergeFileURL = [NSURL fileURLWithPath:[SBCaptureToolKit getVideoMergeFilePathString]];
         
-        AVMutableCompositionTrack *audioTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeAudio preferredTrackID:kCMPersistentTrackID_Invalid];
-        [audioTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, asset.duration)
-                            ofTrack:[[asset tracksWithMediaType:AVMediaTypeAudio] objectAtIndex:0]
-                             atTime:totalDuration
-                              error:nil];
+        //export
+        AVMutableVideoCompositionInstruction *mainInstruciton = [AVMutableVideoCompositionInstruction videoCompositionInstruction];
+        mainInstruciton.timeRange = CMTimeRangeMake(kCMTimeZero, totalDuration);
+        mainInstruciton.layerInstructions = layerInstructionArray;
+        AVMutableVideoComposition *mainCompositionInst = [AVMutableVideoComposition videoComposition];
+        mainCompositionInst.instructions = @[mainInstruciton];
+        mainCompositionInst.frameDuration = CMTimeMake(1, 30);
+        mainCompositionInst.renderSize = CGSizeMake(renderW, renderW);
         
-        AVMutableCompositionTrack *videoTrack = [mixComposition addMutableTrackWithMediaType:AVMediaTypeVideo preferredTrackID:kCMPersistentTrackID_Invalid];
-        
-        [videoTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, asset.duration)
-                            ofTrack:assetTrack
-                             atTime:totalDuration
-                              error:&error];
-        
-        //fix orientationissue
-        AVMutableVideoCompositionLayerInstruction *layerInstruciton = [AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:videoTrack];
-        
-        totalDuration = CMTimeAdd(totalDuration, asset.duration);
-        
-        CGFloat rate;
-        rate = renderW / MIN(assetTrack.naturalSize.width, assetTrack.naturalSize.height);
-        
-        CGAffineTransform layerTransform = CGAffineTransformMake(assetTrack.preferredTransform.a, assetTrack.preferredTransform.b, assetTrack.preferredTransform.c, assetTrack.preferredTransform.d, assetTrack.preferredTransform.tx * rate, assetTrack.preferredTransform.ty * rate);
-        layerTransform = CGAffineTransformConcat(layerTransform, CGAffineTransformMake(1, 0, 0, 1, 0, -(assetTrack.naturalSize.width - assetTrack.naturalSize.height) / 2.0));//向上移动取中部影响
-        layerTransform = CGAffineTransformScale(layerTransform, rate, rate);//放缩，解决前后摄像结果大小不对称
-        
-        [layerInstruciton setTransform:layerTransform atTime:kCMTimeZero];
-        [layerInstruciton setOpacity:0.0 atTime:totalDuration];
-        
-        //data
-        [layerInstructionArray addObject:layerInstruciton];
-    }
-    
-    //get save path
-    NSURL *mergeFileURL = [NSURL fileURLWithPath:[SBCaptureToolKit getVideoMergeFilePathString]];
-    
-    //export
-    AVMutableVideoCompositionInstruction *mainInstruciton = [AVMutableVideoCompositionInstruction videoCompositionInstruction];
-    mainInstruciton.timeRange = CMTimeRangeMake(kCMTimeZero, totalDuration);
-    mainInstruciton.layerInstructions = layerInstructionArray;
-    AVMutableVideoComposition *mainCompositionInst = [AVMutableVideoComposition videoComposition];
-    mainCompositionInst.instructions = @[mainInstruciton];
-    mainCompositionInst.frameDuration = CMTimeMake(1, 30);
-    mainCompositionInst.renderSize = CGSizeMake(renderW, renderW);
-    
-    AVAssetExportSession *exporter = [[AVAssetExportSession alloc] initWithAsset:mixComposition presetName:AVAssetExportPresetMediumQuality];
-    exporter.videoComposition = mainCompositionInst;
-    exporter.outputURL = mergeFileURL;
-    exporter.outputFileType = AVFileTypeMPEG4;
-    exporter.shouldOptimizeForNetworkUse = YES;
-    [exporter exportAsynchronouslyWithCompletionHandler:^{
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if ([_delegate respondsToSelector:@selector(videoRecorder:didFinishMergingVideosToOutPutFileAtURL:)]) {
-                [_delegate videoRecorder:self didFinishMergingVideosToOutPutFileAtURL:mergeFileURL];
-            }
-        });
-    }];
+        AVAssetExportSession *exporter = [[AVAssetExportSession alloc] initWithAsset:mixComposition presetName:AVAssetExportPresetMediumQuality];
+        exporter.videoComposition = mainCompositionInst;
+        exporter.outputURL = mergeFileURL;
+        exporter.outputFileType = AVFileTypeMPEG4;
+        exporter.shouldOptimizeForNetworkUse = YES;
+        [exporter exportAsynchronouslyWithCompletionHandler:^{
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if ([_delegate respondsToSelector:@selector(videoRecorder:didFinishMergingVideosToOutPutFileAtURL:)]) {
+                    [_delegate videoRecorder:self didFinishMergingVideosToOutPutFileAtURL:mergeFileURL];
+                }
+            });
+        }];
+    });
 }
 
 - (AVCaptureDevice *)getCameraDevice:(BOOL)isFront
